447d6f967cec23ef28d65ddb71cfa1d7bf7e8045
AbyChan <crystal.tyan@gmail.com>
2016-06-27 00:06:09 +0800

    initial commit

--- index.ios.js
+++ index.ios.js
@@ -1,37 +1,50 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
-var React = require('react-native');
-var {
+import React, { Component } from 'react';
+let xml = require('react-native').RNMXml;
+import {
   AppRegistry,
   StyleSheet,
+  REACT_DEBUGGER,
   Text,
-  View,
-} = React;
+  View
+} from 'react-native';
 
-var ReactFeed = React.createClass({
-  render: function() {
+class ReactFeed extends Component {
+  constructor() {
+    super();
+    this.fetchFeeds();
+  }
+
+  fetchFeeds() {
+    fetch('https://www.v2ex.com/feed/tab/tech.xml')
+      .then((response) => response.text())
+      .then((responseText) => {
+        console.log(xml);
+        xml.queryXml('<doc a="V1">V2</doc>',
+                     ['/doc/@a', '/doc'],
+                     results => results.map(nodes => console.log(nodes[0])))
+        
+      });
+  }
+  
+  render() {
+    
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to React Native2!
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.ios.js
+          To get started, edit index.android.js
         </Text>
         <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          Shake or press menu button for dev menu
         </Text>
       </View>
     );
   }
-});
+}
 
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
